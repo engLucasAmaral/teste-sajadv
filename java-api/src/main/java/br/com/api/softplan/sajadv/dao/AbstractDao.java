a673cc30875129965f110c9149bfd981fc178b20
Lucas Amaral <eng.lucasamaral@gmail.com>
2020-09-01 23:38:09 -0300

    Ajuste

--- java-api/src/main/java/br/com/api/softplan/sajadv/dao/AbstractDao.java
+++ java-api/src/main/java/br/com/api/softplan/sajadv/dao/AbstractDao.java
@@ -7,42 +7,22 @@ package br.com.api.softplan.sajadv.dao;
 
 import java.io.Serializable;
 import java.util.List;
-import java.util.Map;
 import javax.ejb.TransactionAttribute;
 import javax.ejb.TransactionAttributeType;
 import javax.persistence.EntityManager;
-import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaUpdate;
-import javax.persistence.criteria.Root;
 
 /**
  *
  * @author lucas
  */
-abstract class AbstractDao<T> implements Serializable {
+public abstract class AbstractDao<T> implements Serializable {
 
     protected EntityManager entityManager;
 
     @PersistenceContext
     abstract void setEntityManager(EntityManager entityManager);
-
-    @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
-    public T insert(T t) {
-        return entityManager.merge(t);
-    }
-
-    @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
-    public T update(T t) {
-        return entityManager.merge(t);
-    }
-
-    @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
-    public void remove(T t) {
-        entityManager.remove(t);
-    }
 
     @TransactionAttribute(TransactionAttributeType.NOT_SUPPORTED)
     public T buscarEntidade(String stringQuery, TypeQuery type, Object... params) {
@@ -63,16 +43,27 @@ abstract class AbstractDao<T> implements Serializable {
             setParameters(query, type, params);
         }
         query.setMaxResults(1);
-        try {
-            return (T) query.getSingleResult();
-        } catch (NoResultException e) {
-            return null;
-        }
+        return (T) query.getSingleResult();
     }
 
     @TransactionAttribute(TransactionAttributeType.NOT_SUPPORTED)
     public T buscarEntidade(Class<T> entityClass, Object primaryKey) {
         return entityManager.find(entityClass, primaryKey);
+    }
+
+    @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
+    public T insert(T t) {
+        return entityManager.merge(t);
+    }
+
+    @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
+    public T update(T t) {
+        return entityManager.merge(t);
+    }
+
+    @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
+    public void remove(T t) {
+        entityManager.remove(t);
     }
 
     @TransactionAttribute(TransactionAttributeType.NOT_SUPPORTED)
@@ -134,51 +125,6 @@ abstract class AbstractDao<T> implements Serializable {
             }
             index++;
         }
-    }
-
-    public int atualizarLote(Class<T> entidade, Map<String, Object> fieldsUpdate, List<Long> filtersUpdate) throws Exception {
-        int numRowsAffected = 0;
-        try {
-            CriteriaBuilder builder = entityManager.getCriteriaBuilder();
-            CriteriaUpdate<T> update = builder.createCriteriaUpdate(entidade);
-            Root root = update.from(entidade);
-            fieldsUpdate.entrySet().forEach(obj -> {
-                update.set(obj.getKey(), obj.getValue());
-            });
-            update.where(root.in(filtersUpdate));
-            Query query = entityManager.createQuery(update);
-            numRowsAffected = query.executeUpdate();
-        } catch (Exception e) {
-            throw e;
-        }
-        return numRowsAffected;
-    }
-
-    public int atualizarLote2(T domainClass, Map<String, Object> fieldsUpdate, List<Long> filtersUpdate) throws Exception {
-
-        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
-
-        CriteriaUpdate<T> q = criteriaBuilder.createCriteriaUpdate((Class<T>) domainClass);
-
-        Root<T> root = q.from((Class<T>) domainClass);
-        fieldsUpdate.entrySet().forEach(obj -> {
-            q.set(obj.getKey(), obj.getValue());
-        });
-
-//         update.set(root.get("id"), true).where(root.get("id").in(filtersUpdate.values()));
-//         q./*set(root.get("id"), true).*/where(root.get("id").in(filtersUpdate));// FUNCIONANDO
-        q.where(root.in(filtersUpdate));
-//        final List<Predicate> predicates = new ArrayList<Predicate>();
-////        predicates.add(cb.equal(root.<ID>get("id"), 1));
-////        predicates.add(cb.equal(root.in(filtersUpdate.values())));
-//
-////        update.where(root.in(filtersUpdate.values().toArray()));
-////        update.where(cb.and(predicates.toArray(new Predicate[predicates.size()])));
-//        update.where(root.in(predicates.toArray(new Predicate[predicates.size()])));
-//        update.where(criteriaBuilder.in(root.get(""),filtersUpdate.values().toArray())));
-        int numrow = entityManager.createQuery(q).executeUpdate();
-
-        return numrow;
     }
 
 }
